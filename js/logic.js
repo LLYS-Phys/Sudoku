0df9d4699119da31dbf955a27ad0b0751ea12624
LLYS-Phys <lachyordanov@gmail.com>
2024-03-21 13:19:15 +0200

    Optimize user interaction

--- js/logic.js
+++ js/logic.js
@@ -166,9 +166,9 @@ function drawBoard(board){
 
     for(r=0; r<81; r++){
         cell = document.getElementsByName('num')[r];
-        cell.addEventListener('keydown', check, false);
+        cell.addEventListener('input', check, false);
         if (mistakesToggle){
-            cell.addEventListener('keydown', rules, false);
+            cell.addEventListener('input', rules, false);
         }
     }
 
